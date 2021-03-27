4419fdbbee1e83f76a8cac526012d53a12fa8b92
VitaliiSnitsarchuk <vitaly.snitsarchuk@gmail.com>
2021-03-27 13:12:05 +0200

    changed IstallDocker.sh

--- final-task/InstallDocker.sh
+++ final-task/InstallDocker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-sleep 2
+sleep 5
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update && apt-cache policy docker-ce
-sleep 7
+sleep 11
 sudo apt install -y docker-ce
-sleep 9
+sleep 20
 sudo usermod -aG docker $(whoami)
 exit
 
